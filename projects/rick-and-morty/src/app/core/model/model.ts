badf3936ca0ff75f16bc9758915cc03827f57147
angelo morales <angelotuba3@gmail.com>
2024-04-01 15:34:43 +0200

    Update data model

--- projects/rick-and-morty/src/app/core/model/model.ts
+++ projects/rick-and-morty/src/app/core/model/model.ts
@@ -29,3 +29,10 @@ export interface EpisodesModel {
   air_date: string;
   episode: string;
 }
+
+export interface InfoModel {
+  count: number;
+  pages: number;
+  next: string;
+  prev: string | null;
+}
