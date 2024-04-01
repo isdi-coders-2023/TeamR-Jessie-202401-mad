1e8dcd4c051ddc397fca7b6cccb74e19d886025f
DiegoPertierraM <diegopmenendez@gmail.com>
2024-04-01 17:40:47 +0200

    Add menu component

---
+++ projects/rick-and-morty/src/app/features/shared/menu/menu.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MenuOption } from '../../../core/model/menu-option';
+
+@Component({
+  selector: 'jessie-menu',
+  standalone: true,
+  imports: [RouterModule],
+  template: `
+    <nav>
+      <ul>
+        @for (item of items; track $index) {
+        <li>
+          <a [routerLink]="item.path" routerLinkActive="active">{{
+            item.title
+          }}</a>
+        </li>
+        }
+      </ul>
+    </nav>
+  `,
+  styleUrl: './menu.component.css',
+})
+export class MenuComponent {
+  @Input({
+    required: true,
+  })
+  items: MenuOption[] = [];
+}
