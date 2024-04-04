b191ded60896a64438a172c21ce8bf0c9f5eae49
Kev <kev@Air-de-Kevin.localdomain>
2024-04-04 17:10:51 +0200

    css update and delete-character of character-list testing update

--- projects/rick-and-morty/src/app/features/character-list/character-list.component.spec.ts
+++ projects/rick-and-morty/src/app/features/character-list/character-list.component.spec.ts
@@ -1,24 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CharacterListComponent } from './character-list.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CharacterCardComponent } from '../character-card/character-card.component';
+import { StateService } from '../../core/services/state.service';
 
 describe('CharacterListComponent', () => {
   let component: CharacterListComponent;
   let fixture: ComponentFixture<CharacterListComponent>;
+  let mockServ: jasmine.SpyObj<StateService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CharacterListComponent, HttpClientTestingModule],
+  beforeEach(() => {
+    mockServ = jasmine.createSpyObj<StateService>(['deleteCharacter']);
+
+    TestBed.configureTestingModule({
+      imports: [CharacterListComponent, CharacterCardComponent],
+      providers: [{ provide: StateService, useValue: mockServ }],
     }).compileComponents();
+
 
     fixture = TestBed.createComponent(CharacterListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  // it('should render a title received as input'), {};
-});
+  it('should call stateSrv.deleteCharacter with correct id', () => {
+      const characterId = 1;
+      component.characterList = [];
+      component.deleteCharacter(characterId);
+      expect(mockServ.deleteCharacter).toHaveBeenCalled();
+    });
+})
