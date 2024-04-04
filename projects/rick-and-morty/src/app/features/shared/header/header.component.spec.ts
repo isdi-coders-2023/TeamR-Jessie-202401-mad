e1f75a5832956ce0d0b839a0e81fa1223d6428cc
DiegoPertierraM <diegopmenendez@gmail.com>
2024-04-04 10:13:37 +0200

    Add header test

--- projects/rick-and-morty/src/app/features/shared/header/header.component.spec.ts
+++ projects/rick-and-morty/src/app/features/shared/header/header.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 import { provideRouter } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MenuOption } from '../../../core/model/menu-option';
 
-describe('HeaderComponent', () => {
+fdescribe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
+  const mockMenuOptions: MenuOption[] = [
+    { title: 'Characters', path: '/characters' },
+    { title: 'Episodes', path: '/episodes' },
+    { title: 'Locations', path: '/locations' },
+  ];
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent],
+      imports: [HeaderComponent, HttpClientTestingModule],
       providers: [provideRouter([])],
     }).compileComponents();
 
@@ -19,5 +27,41 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle menu visibility on click', () => {
+    expect(component.menuVisible).toBeFalsy();
+
+    const icon = fixture.nativeElement.querySelector('.ham-menu');
+    icon.click();
+    fixture.detectChanges();
+
+    expect(component.menuVisible).toBeTruthy();
+
+    icon.click();
+    fixture.detectChanges();
+
+    expect(component.menuVisible).toBeFalsy();
+  });
+
+  it('should display correct menu options', () => {
+    const menu = fixture.nativeElement.querySelector('.header-menu');
+    const menuItems = menu.querySelectorAll('li');
+
+    expect(menuItems.length).toBe(mockMenuOptions.length);
+
+    menuItems.forEach((menuItem: HTMLElement, index: number) => {
+      expect(menuItem.textContent?.trim()).toBe(mockMenuOptions[index].title);
+    });
+  });
+
+  it('should toggle menu visibility on Enter key press', () => {
+    expect(component.menuVisible).toBeFalsy();
+
+    const icon = fixture.nativeElement.querySelector('.ham-menu');
+    icon.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter' }));
+    fixture.detectChanges();
+
+    expect(component.menuVisible).toBeTruthy();
   });
 });
