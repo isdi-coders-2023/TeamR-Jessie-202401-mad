ea6ea916fc697d24b13e63484602f43b49f213ff
Kev <kev@MacBook-Air-de-Kevin.local>
2024-04-06 17:51:35 +0200

    error page finished w testing and css

--- projects/rick-and-morty/src/app/features/error-page/error-page.component.spec.ts
+++ projects/rick-and-morty/src/app/features/error-page/error-page.component.spec.ts
@@ -1,22 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import ErrorPageComponent from './error-page.component';
+import { By } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('ErrorPageComponent', () => {
+fdescribe('ErrorPageComponent', () => {
   let component: ErrorPageComponent;
   let fixture: ComponentFixture<ErrorPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ErrorPageComponent],
+      imports: [ErrorPageComponent, HttpClientModule, RouterTestingModule],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(ErrorPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+ 
+  fixture = TestBed.createComponent(ErrorPageComponent);
+  component = fixture.componentInstance; 
+  fixture.detectChanges();
+ })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  it('should call action when click on img',()=>{
+    spyOn(component, 'goBack');
+    const img = fixture.debugElement.queryAll(By.css('img'))
+    img[0].triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.goBack).toHaveBeenCalled
+  })
+
+  it('should call action when click on text',()=>{
+    spyOn(component, 'goBack');
+    const button = fixture.debugElement.queryAll(By.css('button'))
+    button[0].triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.goBack).toHaveBeenCalled
+  })
+})
--- projects/rick-and-morty/src/app/features/error-page/error-page.component.ts
+++ projects/rick-and-morty/src/app/features/error-page/error-page.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../shared/header/header.component";
+import { FooterComponent } from "../shared/footer/footer.component";
 
 @Component({
-  selector: 'jessie-error-page',
-  standalone: true,
-  imports: [],
-  template: ` <div class="error-message">
-    <img src="./assets/img/error.png" alt="error img" />
-    <h2>ERROR</h2>
-    <p>We Couldn't find the page you were looking for</p>
-    <a href="index.html"
-      ><img src="./assets/img/img-go-back.png" alt="img btn back" width="100" />
-      GO BACK</a
-    >
-  </div>`,
-  styleUrl: './error-page.component.css',
+    selector: 'jessie-error-page',
+    standalone: true,
+    template: ` 
+  <jessie-header />
+    <section>
+      <div class="error-img">
+        <img  src="./assets/img/error.png" alt="error img" />
+      </div>
+      <div>
+        <div class="error-message">
+          <h2>ERROR</h2>
+          <p>We Couldn't find the page you were looking for</p>
+        </div>
+        <div class="error-button">
+          <img (click)="goBack()" src="./assets/img/img-go-back.png" alt="img btn back"  />
+          <button (click)="goBack()">GO BACK</button>
+        </div>
+      </div>
+    </section>
+  <jessie-footer />`,
+    styleUrl: './error-page.component.css',
+    imports: [HeaderComponent, FooterComponent]
 })
-export default class ErrorPageComponent {}
+export default class ErrorPageComponent {
+  goBack() {
+    window.location.href = 'index.html';
+}
+}
